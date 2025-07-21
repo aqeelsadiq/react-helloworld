9fa8076ed8613b138dd6ac0b4efd3cf35fdeb800
Muhammad Aqeel Sadiq <114500109+aqeelsadiq@users.noreply.github.com>
2025-07-21 23:32:42 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>simon, helloworld!!! this is dev branch</div>
+      <div>simon, helloworld!!! this is master branch</div>
     );
   }
 }
